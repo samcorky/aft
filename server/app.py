d7501ffb4fd31025eda40b2ca4c05aee0c61e2d6
Stephen Jefferson <stephen@sjefferson.co.uk>
2025-12-06 12:08:54 +0000

    Bump version

--- server/app.py
+++ server/app.py
@@ -21,7 +21,7 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 # Application version
-APP_VERSION = "1.2.0"
+APP_VERSION = "1.2.1"
 
 # Settings schema - defines allowed settings and their validation rules
 SETTINGS_SCHEMA = {
